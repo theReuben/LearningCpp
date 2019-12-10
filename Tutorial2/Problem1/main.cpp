b0214d643898df4b330ad87175e44bc03714d653
Reuben Day <reubenday@btinternet.com>
2019-12-10 20:51:04 +0000

    Done Tut2 Prob1

---
+++ Tutorial2/Problem1/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+	std::string sAge;
+	int iAge;
+	std::cout << "Enter age : ";
+	getline(std::cin, sAge);
+	iAge = std::stoi(sAge);
+	if (iAge < 5) {
+		printf("Too young for school.\n");
+	} else if (iAge == 5) {
+		printf("Go to kindergarten.\n");
+	} else if (iAge >= 6 && iAge <= 17) {
+		printf("Go to grade %d\n", iAge-5);
+	} else {
+		printf("Go to college.\n");
+	}
+	return 0;
+}
