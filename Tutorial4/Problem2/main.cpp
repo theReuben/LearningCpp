1102bcf2b6794e02c22d0745452c6eba06a62d80
Reuben Day <reubenday@btinternet.com>
2020-01-30 15:55:55 +0000

    try catch example

---
+++ Tutorial4/Problem2/main.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <numeric>
+
+int divide(double num1, double num2);
+
+int main(int argc, char const *argv[])
+{
+	int num1, num2;
+	std::cout << "Enter numerator: ";
+	std::cin >> num1;
+	std::cout << "Enter denominator: ";
+	std::cin >> num2;
+	divide(num1, num2);
+	return 0;
+}
+
+int divide(double num1, double num2) {
+
+	try {
+		if (num2 == 0) {
+			throw "Divide by zero exception.";
+		} else {
+			printf("%.1f / %.1f = %.1f\n", num1, num2, (num1/num2));;
+		}
+	} catch(const char* exp) {
+		std::cout << "Thrown: " << exp << "\n";
+	}
+	return 0;
+}
