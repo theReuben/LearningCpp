76260b7a0e95d89584daa8983ad146939e5ca028
Reuben Day <reubenday@btinternet.com>
2020-02-02 19:53:51 +0000

    Replaced with recursive merge sort

--- Tutorial8/Sorting/Merge/main.cpp
+++ Tutorial8/Sorting/Merge/main.cpp
@@ -7,8 +7,8 @@
 #include <ctime>
 #include <stddef.h>
 
-void mergeSort(std::vector<int>& v);
-std::vector<int> MergeArrays(std::vector<int>& v1, std::vector<int>& v2);
+template<typename T> void merge_sort(T& v);
+template<typename T> void merge(T& left, T& right, T& v);
 
 int main(int argc, char const *argv[]) {
 
@@ -16,46 +16,45 @@ int main(int argc, char const *argv[]) {
 	std::vector<int> v{ 9,3,4,2,5,7,2,8 };
 	for(auto elem : v) std::cout << elem << ", ";	
 	std::cout << "\n";
-	mergeSort(v);
+	merge_sort(v);
 	return 0;
 }
 
-void mergeSort(std::vector<int>& v) {
+template<typename T> 
+void merge_sort(T& v) {
 	clock_t time = clock();
-	std::vector<std::vector<int>> v3;
-	for (size_t i = 0; i < v.size(); i++) {
-		std::vector<int> vec {v[i]};
-		v3.push_back(vec);
+	if (v.size() <= 1) {
+		return;
 	}
-	while(v3.size() > 1) {
-		std::vector<int> v1 {v3[0]};
-		std::vector<int> v2 {v3[1]};
-		v3.erase(v3.begin(), v3.begin() + 2);
-		v3.push_back(MergeArrays(v1, v2));
-	}
-	v = (std::vector<int>) v[0];
+	int mid = (v.size() + 1)/2;
+	T left;
+	T right;
+	std::copy(v.begin(), v.begin()+mid, std::back_inserter(left));
+	std::copy(v.begin()+mid, v.end(), std::back_inserter(right));
+
+	merge_sort(left);
+	merge_sort(right);
+	merge(left, right, v);
+	for(auto elem : v) std::cout << elem << ", ";
+	std::cout << "\n";
 	time = clock() - time;
 	std::cout << "Runtime: " << (float)time/CLOCKS_PER_SEC << std::endl;
 }
 
-// Currently returns new array, would like to convert it so inplace merge
-std::vector<int> MergeArrays(std::vector<int>& v1, std::vector<int>& v2) {
-	std::vector<int> v3;
-	int v1_i = 0;
-	int v2_i = 0;
-	while(v1_i < v1.size() && v2_i < v2.size()) {
-		if (v1[v1_i] <= v2[v2_i]) {
-			v3.push_back(v1[v1_i]);
-			v1_i += 1;
-		} else {
-			v3.push_back(v2[v2_i]);
-			v2_i += 1;
-		}
+template<typename T> 
+void merge(T& left, T& right, T& v) {
+	size_t nL = 0;
+	size_t nR = 0;
+	size_t n = 0;
+
+	while (nL < left.size() && nR < right.size()) {
+		v[n++] = (left[nL] <= right[nR]) ? left[nL++] : right[nR++];
 	}
-	if(v1_i < v1.size()) {
-		while (v1_i < v1.size()) v3.push_back(v1[v1_i++]);
-	} else if (v2_i < v2.size()) {
-		while (v2_i < v2.size()) v3.push_back(v2[v2_i++]);
-	}	
-	return v3;
+
+	while (nL < left.size()) {
+		v[n++] = left[nL++];
+	}
+	while (nR < right.size()) {
+		v[n++] = right[nR++];
+	}
 }
